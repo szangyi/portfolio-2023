710183267773723d917d9fb72621d1e962c4aac9
Alexandra Mészáros <alexandra.mszrs@gmail.com>
2023-01-04 20:21:18 +0100

    project cards added

--- js/main.js
+++ js/main.js
@@ -15,16 +15,58 @@ document.getElementById('flip-card').addEventListener('click', function() {
 
 
 // NAME TEXT ANIMATION
-var textWrapper = document.querySelector('.namep .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+function moveName() {
+    var textWrapper = document.querySelector('.namep .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    
+    setTimeout(() => {
+        anime.timeline({ loop: false })
+            .add({
+                targets: '.namep .letter',
+                translateY: ["1.1em", 0],
+                translateZ: 0,
+                duration: 750,
+                delay: (el, i) => 50 * i
+            })
+    }, 2000);
+}
 
-setTimeout(() => {
-    anime.timeline({ loop: false })
-        .add({
-            targets: '.namep .letter',
-            translateY: ["1.1em", 0],
-            translateZ: 0,
-            duration: 750,
-            delay: (el, i) => 50 * i
-        })
-}, 1000);
+moveName(); 
+setInterval(moveName, 10000);  // every 1 minute
+
+
+
+
+// PROJECT TILES
+var $cell = $('.card');
+
+//open and close card when clicked on card
+$cell.find('.js-expander').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  if ($thisCell.hasClass('is-collapsed')) {
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+    
+    if ($cell.not($thisCell).hasClass('is-inactive')) {
+      //do nothing
+    } else {
+      $cell.not($thisCell).addClass('is-inactive');
+    }
+
+  } else {
+    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+    $cell.not($thisCell).removeClass('is-inactive');
+  }
+});
+
+//close card when click on cross
+$cell.find('.js-collapser').click(function() {
+
+  var $thisCell = $(this).closest('.card');
+
+  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  $cell.not($thisCell).removeClass('is-inactive');
+
+});
